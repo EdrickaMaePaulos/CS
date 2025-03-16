41ef05ee54152778fce73c2295f769b460d89a0a
Edricka Mae <157472381+EdrickaMaePaulos@users.noreply.github.com>
2025-03-16 21:46:27 +0800

    Add files via upload

---
+++ DigitProduct/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class DigitProduct{
+    static void Main(){
+        Console.Write("Enter N: ");
+        int n = int.Parse(Console.ReadLine());
+        int product = 1;
+        
+        while (n > 0){
+            int digit = n % 10;
+            if (digit != 0){
+                product *= digit;
+            }
+            n /= 10;
+        }
+        Console.WriteLine("Digit Product: " + product);
+    }
+}
---
+++ SkippingRows/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+Console.Write("Enter n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+        if (i == y)
+        {
+for (int i = 1; i <= n; i++)
+{
+    for (int j = 1; j <= n; j++)
+    {
+            continue;
+        }
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
