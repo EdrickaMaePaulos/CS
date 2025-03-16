a173ab07c5aa16ba88e3644f9823b665b0f26c39
Edricka Mae <157472381+EdrickaMaePaulos@users.noreply.github.com>
2025-03-16 21:44:38 +0800

    Add files via upload

---
+++ Boolean/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+int num1 = 5;
+int num2 = 10;
+        
+// Check if both num1 and num2 are greater than 0 using &&
+bool bothPositive = (num1 > 0) && (num2 > 0);
+
+Console.WriteLine($"Are both numbers positive? {bothPositive}");
+
+// Are both numbers positive? True
+
+
+
+bool isWindyDay = false;
+
+// Use the NOT operator (!) to reverse the boolean value
+bool isCloudyDay = !isWindyDay;
+
+Console.WriteLine($"Is today a cloudy day? {isCloudyDay}");
+
+// Is today a cloudy day? True
+
+
+
+
+num1 = -5;
+num2 = -10;
+        
+// Check if either num1 or num2 is greater than 0 using ||
+bool eitherPositive = (num1 > 0) || (num2 > 0);
+
+Console.WriteLine($"Is either number positive? {eitherPositive}");
+
+// Is either number positive? True
---
+++ FindingLCM/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class LCM{
+    
+    static void Main(){
+        Console.Write("Enter number of inputs: ");
+        int n = int.Parse(Console.ReadLine());
+        int[] numbers = new int[n];
+        
+        Console.Write($"Enter {n} inputs: ");
+        string inputs = Console.ReadLine();
+        string[] inputStrings = inputs.Split(' ');
+        
+        
+        for (int i = 0; i < n; i++){
+            numbers[i] = int.Parse(inputStrings[i]);
+        }
+        int lcm = CalculatedLCM(numbers);
+        Console.WriteLine("LCM = " + lcm);
+        
+    }
+    
+    public static int CalculatedLCM(int[] numbers){
+        
+        if (numbers.Length == 0){
+            return 1;
+        }
+        
+        int result = numbers[0];
+        for (int i = 1; i < numbers.Length; i++){
+            result = FindLCM(result, numbers[i]);
+        }
+        return result;
+    }
+    
+    
+    public static int FindLCM(int a, int b){
+        return (a * b) / FindGCD(a, b);
+    }
+    
+    public static int FindGCD(int a, int b){
+        while (b != 0){
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
